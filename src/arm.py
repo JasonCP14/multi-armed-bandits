52a1ab63079c1fea4a13a8d69a033ff21b31da75
unknown <jasoncp61@gmail.com>
2023-09-04 18:09:36 +0800

    Add docstrings

--- src/arm.py
+++ src/arm.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 class Arm:
-    """ The Arm class 
+    """ The Arm class.
     
-    Args:
-        mean (float): True mean of the arm's distribution
-        var (float): True variance of the arm's distribution
+    Attributes:
+        mean (float): True mean of the arm's distribution.
+        var (float): True variance of the arm's distribution.
     """
     
     def __init__(self, mean: float):                
@@ -14,17 +14,17 @@ class Arm:
         self.initialize()
         
     def initialize(self) -> None:
-        """ Initialize the miu, sigma and number of pulls """
+        """ Initializes the miu, sigma and number of pulls. """
 
         self.miu = 0 
         self.sigma_sqr = 1      # TODO: change              
         self.num_pulls = 0  
     
     def pull(self) -> float:
-        """ Pulls this arm to get a reward from the true distribution
+        """ Pulls this arm to get a reward from the true distribution.
 
         Returns:
-            float: The arm's reward
+            float: The arm's reward.
         """
 
         value = np.random.randn(self.mean, np.sqrt(self.variance))   
--- src/expected_improvement.py
+++ src/expected_improvement.py
@@ -5,13 +5,22 @@ from typing import List
 from scipy.stats import bernoulli
 
 class ExpectedImprovement:
+    """ The Expected Improvement method class.
+    
+    Attributes:
+        arms (List[Arm]): List of arms in the current problem.
+        beta (float): Beta hyperparameter to choose amongst the top two.
+        max_iters (int): Number of iterations that the method can go through.
+    """
 
     def __init__(self, arms: List[Arm], beta: float, max_iters: int):
         self.arms = arms
         self.beta = beta
         self.max_iters = max_iters
         
-    def run(self):
+    def run(self) -> None:
+        """ Runs the method. """
+
         for i in range(self.max_iters):
             leader = self.get_leader()
             challenger = self.get_challenger(leader)
@@ -19,7 +28,13 @@ class ExpectedImprovement:
             reward = chosen_arm.pull()
             self.update(chosen_arm, reward)
 
-    def get_leader(self):
+    def get_leader(self) -> Arm:
+        """ Gets the leader based on the EI sampling.
+
+        Returns:
+            Arm: The leader.
+        """
+
         target = self.get_highest_posterior_mean()
         leader, leader_value = None, -math.inf
         for arm in self.arms:
@@ -30,7 +45,16 @@ class ExpectedImprovement:
 
         return leader
 
-    def get_challenger(self, leader):
+    def get_challenger(self, leader: Arm) -> Arm:
+        """ Gets the challenger based on the EI sampling.
+
+        Args:
+            leader (Arm): The leader to challenge.
+
+        Returns:
+            Arm: The challenger.
+        """
+
         challenger, challenger_value = None, -math.inf
         for arm in self.arms:
             if arm is not leader:
@@ -41,11 +65,23 @@ class ExpectedImprovement:
 
         return challenger
 
-    def update(arm, reward):
+    def update(arm, reward: float) -> None:
+        """ Updates the chosen arm according to the pulled reward.
+
+        Args:
+            reward (float): The reward pulled from the true distribution.
+        """
+
         arm.miu = (arm.miu/arm.sigma_sqr + reward/arm.variance) / (1/arm.sigma_sqr + 1/arm.variance)
         arm.sigma = 1/(1/arm.sigma_sqr + 1/arm.variance)
 
     def get_highest_posterior_mean(self) -> Arm:
+        """ Gets the arm with the highest posterior mean.
+
+        Returns:
+            Arm: The arm with the highest posterior mean.
+        """
+
         return max(self.arms, key = lambda x: x.miu)
     
     def f(self, x):
